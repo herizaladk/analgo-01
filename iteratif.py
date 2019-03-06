63c4776be152441ce577a0c2551ae8b5e947bda1
herizaladk <herizaladk@gmail.com>
2019-03-06 14:11:13 +0700

    Analisa Algoritma 1

---
+++ iteratif.py
@@ -0,0 +1,10 @@
+def iterasi(n):
+
+    f = 2
+    for i in range(1,n+1):
+        x = f ** i  
+        print (x)
+
+print (iterasi(5))
+
+
---
+++ rekursi.py
@@ -0,0 +1,7 @@
+def rekursi (n):
+    if(n == 0):
+        return 1 
+    else:
+        return 2**n
+
+print(rekursi(5))
